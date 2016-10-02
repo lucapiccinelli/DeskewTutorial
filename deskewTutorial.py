88b15d65da66af9d0630c3cae138231f2450c8d2
Luca Piccinelli <Luca Piccinelli>
2016-10-02 23:53:48 +0200

    lettura e visualizzazione dell'immagine di esempio

--- deskewTutorial.py
+++ deskewTutorial.py
@@ -1,14 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 15 21:29:14 2016
-
-@author: luca_
-"""
-
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
 
 im = cv2.imread(r'img\text.jpg', cv2.IMREAD_GRAYSCALE)
-        
+
 plt.imshow(im, cmap='Greys_r')
