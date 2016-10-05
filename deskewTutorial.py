908039c29f0d87e7f47f2079dc91418dba57ebc4
Luca Piccinelli <Luca Piccinelli>
2016-10-05 22:31:37 +0200

    preprocessing: ridimensionamento e canny edge detector

--- deskewTutorial.py
+++ deskewTutorial.py
@@ -1,6 +1,14 @@
 import cv2
 import matplotlib.pyplot as plt
 
+#caricamento dell'immagine da disco
 im = cv2.imread(r'img\text.jpg', cv2.IMREAD_GRAYSCALE)
+
+h, w = im.shape
+#ridimensionamento dell'immagine
+im = cv2.resize(im, (w/4,h/4))
+
+#canny edge detector
+im = cv2.Canny(im, 100, 200)
 
 plt.imshow(im, cmap='Greys_r')
