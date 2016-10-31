3bd7c87783605a671c3e07c0c9ef764426233f2d
Luca Piccinelli <Luca Piccinelli>
2016-10-31 21:51:46 +0100

    avevo lasciato il ridimensionamento a 1/16

--- deskewTutorial.py
+++ deskewTutorial.py
@@ -57,7 +57,7 @@ im = cv2.imread(r'img\text.jpg', cv2.IMREAD_GRAYSCALE)
 
 h, w = im.shape
 #ridimensionamento dell'immagine
-im = cv2.resize(im, (w/4,h/4))
+im = cv2.resize(im, (w/3,h/3))
 
 #canny edge detector
 im = cv2.Canny(im, 100, 200)
